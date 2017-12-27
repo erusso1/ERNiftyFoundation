967e25c09367777330560d010742d86fd1c57d82
Ephraim Russo <ephraim.s.russo@gmail.com>
2017-12-27 00:25:39 -0800

    Add text color as parameter for configuration of terms text view.

--- ERNiftyFoundation/Source/Classes/ERTermsOfUseTextView.swift
+++ ERNiftyFoundation/Source/Classes/ERTermsOfUseTextView.swift
@@ -20,13 +20,13 @@ public class ERTermsOfUseTextView: UITextView {
     super.init(coder: aDecoder)
   }
   
-  public func configureFor(termsOfUseURL: URL, privacyPolicyURL: URL, productName: String) {
+  public func configureFor(termsOfUseURL: URL, privacyPolicyURL: URL, productName: String, textColor: UIColor = .black) {
     
     let paragraphStyle = NSMutableParagraphStyle()
     
     paragraphStyle.alignment = .center
     
-    let string = NSMutableAttributedString(string: "By continuing I agree to the \(productName) terms of use and privacy policy.", attributes: [.paragraphStyle : paragraphStyle, .foregroundColor : UIColor.lightGray])
+    let string = NSMutableAttributedString(string: "By continuing I agree to the \(productName) terms of use and privacy policy.", attributes: [.paragraphStyle : paragraphStyle, .foregroundColor : textColor])
     
     string.setAsLink(textToFind: "terms of use", linkURL: termsOfUseURL)
     
